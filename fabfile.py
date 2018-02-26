c2298d1a10cc0efe7134b6b879b23d7f54d712a6
Logos <admin@attacker.club>
2018-02-26 20:33:17 +0800

    auto-up

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,8 @@ from fabric.api import local, lcd
 
 
 def git():
+        local('git pull')
         local('git add .')
-        local('git commit -m "update"')
+        local('git commit -m "auto-up"')
         local('git push')
-        local('git add .')
 
