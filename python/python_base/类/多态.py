f742ef5943c1763c9fe3a3bccb15c797ef368a99
Logos <admin@attacker.club>
2018-02-27 00:39:52 +0800

    auto-up

---
+++ python/python_base/类/多态.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# --------------------------------------------------
+#Author:    LJ
+#Email:     admin@attacker.club
+
+#Date:      18-2-27
+#Description:
+# --------------------------------------------------
---
+++ python/python_base/类/封装.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# --------------------------------------------------
+#Author:    LJ
+#Email:     admin@attacker.club
+
+#Date:      18-2-27
+#Description:
+# --------------------------------------------------
+
